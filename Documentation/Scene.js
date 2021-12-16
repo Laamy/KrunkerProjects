34a21fda7617020babd9b2d85c519250cba31c89
Laamy <83557555+Laamy@users.noreply.github.com>
2021-12-17 00:28:24 +1100

    Create Scene.js

---
+++ Documentation/Scene.js
@@ -0,0 +1,8 @@
+// Documentation continues off three.js documentation
+// https://threejs.org/docs/#api/en/scenes/Scene
+
+class Scene { // Remember to set all StaticMeshes to dynamic before editing!!
+	const plrArray = []; // All Object3D's
+	const propsArray = []; // All Meshes's
+	const mapArray = []; // All StaticMesh's (Meaning you cant modify them!)
+};
