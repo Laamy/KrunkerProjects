d9ea03a9ea68fc865ce1c7c6287faa69bd9022ab
Laamy <83557555+Laamy@users.noreply.github.com>
2021-12-19 01:19:03 +1100

    Create Player.js

---
+++ Documentation/Player.js
@@ -0,0 +1,21 @@
+class Player {
+	0: PlayerBody {
+		0: leg (LeftLeg)
+		1: leg (RightLeg)
+		2: leg (LeftCrouchedLeg)
+		3: leg (RightCrouchedLeg)
+		4: Nameless (UpperBody) {
+			0: head (Head)
+			1: body (Torso)
+			2: Nameless (PlayerAttributes) {
+				0: Nameless (ArmAnimations) {
+					0: Nameless (ArmModel)
+					1: Nameless (GunModel)
+					2: Nameless (Unknown3)
+				}
+				1: Nameless (Unknown2)
+				2: Nameless (Unknown3)
+			}
+		}
+	}
+}
