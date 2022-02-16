405f2b91935119d34832fd29809faa7df47a7f0b
yeemi routh <shade4keal@gmail.com>
2022-02-16 20:11:39 +1100

    Little edits so I dont have to deal with the library functions in VS

--- Netherite.IO/script.js
+++ Netherite.IO/script.js
@@ -26,7 +26,7 @@ entityLoop.rotation.order = 'YXZ';
 const geometry = new THREE.CylinderGeometry(5, 5, 20, 64);
 const material = new THREE.MeshLambertMaterial({ color: 'red', depthTest: false, renderOrder: Infinity, transparent: true, opacity: 0.25, });
 
-// UI Library
+//#region UI Library
 function createPanel(x, y, sx, sy, color){
 	var panelTest = document.createElement('div'); // panel
 	panelTest.style.backgroundColor = color;
@@ -35,8 +35,7 @@ function createPanel(x, y, sx, sy, color){
 	panelTest.style.left = x + 'px';
 	panelTest.style.top = y + 'px';
 	panelArray.push(panelTest);
-	document.body.appendChild(panelTest);
-};
+	document.body.appendChild(panelTest); };
 function createBtn(x, y, text, color, onPressed){
 	var buttonTest = document.createElement('button');
 	buttonTest.style.position = 'absolute';
@@ -48,8 +47,7 @@ function createBtn(x, y, text, color, onPressed){
 	buttonTest.style.top = y + 'px';
 	buttonTest.style.left = x + 'px';
 	btnArray.push(buttonTest);
-	document.body.appendChild(buttonTest);
-};
+	document.body.appendChild(buttonTest); };
 function createLab(x, y, text, color){
 	var labelTest = document.createElement('div');
 	labelTest.style.position = 'absolute';
@@ -60,12 +58,10 @@ function createLab(x, y, text, color){
 	labelTest.style.top = y + 'px';
 	labelTest.style.left = x + 'px';
 	labArray.push(labelTest);
-	document.body.appendChild(labelTest);
-};
+	document.body.appendChild(labelTest); };
 
 // Module Library
 function addModule(name, desc, keybind, category, enabled) {
-	
 	let moduleArray = [];
 	moduleArray.push(name);
 	moduleArray.push(desc);
@@ -73,9 +69,7 @@ function addModule(name, desc, keybind, category, enabled) {
 	moduleArray.push(category);
 	moduleArray.push(enabled);
 	
-	modules.push(moduleArray);
-	
-}
+	modules.push(moduleArray); };
 function getModuleByName(name) {
 	
 	for (let i = 0; i < modules.length; i ++)
@@ -86,12 +80,10 @@ function getModuleByName(name) {
 		{
 			return moduleArray;
 		}
-	}
-	
-}
+	} };
 
 addModule("TestModule", "Module used for testing", 0x07, "Misc", false);
 
 let testModule = getModuleByName("TestModule");
 
-createLab(255, 0, testModule[1], "transparent");
+createLab(255, 0, testModule[1], "transparent"); // worked
